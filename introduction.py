8f8a5ad826fb1c73110e4d17f2b3aedd61ff77c5
Manh Hung <>
2025-04-14 22:02:27 +0700

    introduction

---
+++ introduction.py
@@ -0,0 +1,39 @@
+# 1. Logic & syntax in programing
+
+
+# Tưởng tượng bạn chuẩn bị ra chiến trường trong 1 cuộc chiến tranh, 2 thứ bạn cần là gì ?
+# Vũ khí và chiến thuật. 2 thứ này không thể thiếu và luôn bổ trợ cho nhau.
+# Nếu bạn có 1 vu khí tốt mà lại không có chiến thuật hay. Thì chắc chắn sẽ bị thất bại.
+# Hay quay lại về với lập trình
+# Vũ khí: Syntax, cú pháp luyện tập để sử dụng nhuẫn nhuyễn
+# Chiến thuật: Logic,
+# Nguyên tắc 4060: 40% syntax và 60% logic.
+
+# Logic là gì ?
+# Cách giải quyết vấn
+# Thuật toán (algorithms)
+# Hiểu cách xử lí của chương trình
+# Biểu diên ý hiểu của 1 tiền trình
+
+
+# 2. Software development life cycle (SDLC)
+# Waterfall model:
+# Planning -> analysic -> design -> implementations -> maintenance
+# a, Details:
+# - Planning:
+# Tìm ra 1 vấn đề và tìm cách xử lí nó,
+# ví dụ: bạn có 1 vấn đề và cần làm 1 website để xử lí nó
+# thì website đó là loại website gì? bao gồm nhưng chức năng gì ? Cần công cụ gì ?
+# rất nhiều các câu hỏi cần phải giải quyết. What? how ? why
+# - Analysic:
+# Ở đây bạn sẽ phải trả lời các câu hỏi nên hay không nên, có hoặc không ?
+# Tôi nên tạo một website với function a, b, c ? đúng bạn nên tạo
+# cái bạn cần cái bạn không ? Tập trung vào những cái giá trị
+# - Design:
+# Chuẩn bị code, bao gồm các câu hỏi ? code web thì nên dùng gì ? 
+# các bài toán a và b này thì có công nghệ gì để xử lí ?
+# - Implementation:
+# Bắt đầu code, fix bug ....
+# - Maintenance:
+# Monitoring
+# Duy trì update (feature, công nghệ...)
